c09156bed44f366befd7c3cd4ab1acb2d90f38ed
Tarek Hamaoui <tarek.m.hamaoui@gmail.com>
2022-07-20 13:54:08 +0200

    *clean up

--- __tests__/app.unit.spec.js
+++ __tests__/app.unit.spec.js
@@ -16,24 +16,10 @@ describe("App methods", () => {
 
     App.methods.refresh();
     console.log(before_data);
-
     const after_data = App.data();
-
     expect(after_data).not.toBe(before_data);
-    // expect(after_data).toContain("4");
-    const x1 = after_data.x1;
     const x2 = after_data.x2;
-    console.log(after_data);
-    console.log(x1);
-    console.log(x2);
-
-    console.log(typeof x2);
     const string = x2.toString();
-    console.log(string);
-    console.log(typeof string);
-
     expect(string).toEqual(expect.not.stringContaining(","));
-
-    // console.log(after_data);
   });
 });
